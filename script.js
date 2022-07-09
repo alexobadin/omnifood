2e60f2259298ca9dc5d259f0e778535e71df9d9a
Alex Obadin <ialexobadin@gmail.com>
2022-07-09 20:41:27 +0300

    add amazing sticky nav-bar

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/////////// open/close menu btn
 const modal = document.querySelector('.header ');
 const openBtn = document.getElementById('open-btn');
 const closeBtn = document.getElementById('close-btn');
@@ -16,3 +17,27 @@ const closeModal = function () {
 openBtn.addEventListener('click', openModal);
 closeBtn.addEventListener('click', closeModal);
 document.querySelector('.header-nav').addEventListener('click', closeModal);
+
+//////////// sticky header
+
+const sectionHeroEl = document.querySelector('.hero-section');
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add('sticky');
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove('sticky');
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: '-80px',
+  }
+);
+obs.observe(sectionHeroEl);
